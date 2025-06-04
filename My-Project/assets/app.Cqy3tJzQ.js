eafa82ec77330f28f450c3ac37007e15aa63fe08
李佐群 <2645929971@qq.com>
2025-06-04 17:48:26 +0800

    feat(myProject): ci 部署

--- My-Project/assets/app.Cqy3tJzQ.js
+++
@@ -1,7 +0,0 @@
-import{j as o,aT as p,aU as u,aV as l,aW as c,aX as f,aY as d,aZ as m,a_ as h,a$ as A,b0 as g,Y as v,d as P,u as b,l as w,z as y,b1 as C,b2 as R,b3 as _,aE as E}from"./chunks/framework.Dn4syfiR.js";import{R as T}from"./chunks/theme.BOifr1cl.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const s=i(T),D=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=b();return w(()=>{y(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&C(),R(),_(),s.setup&&s.setup(),()=>E(s.Layout)}});async function j(){const e=O(),a=L();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function L(){return h(D)}function O(){let e=o,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&j().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{j as createApp};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
