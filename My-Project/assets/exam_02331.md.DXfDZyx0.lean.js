9eb697584561e34da1e7a28a0d895ab132e63915
modify <2645929971@qq.com>
2025-08-14 15:36:30 +0800

    ci(my-project): update & ci

---
+++ My-Project/assets/exam_02331.md.DXfDZyx0.lean.js
@@ -0,0 +1,6 @@
+import{_ as h,C as a,c as e,o as k,aP as n,G as i}from"./chunks/framework.BJOOWd-7.js";const o=JSON.parse('{"title":"02331 数据结构 ✒️","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"exam/02331.md","filePath":"exam/02331.md","lastUpdated":1755156737000}'),r={name:"exam/02331.md"};function E(d,s,c,g,m,y){const l=a("MarkMermaid"),p=a("BackTop"),t=a("MarkTitle");return k(),e("div",null,[s[0]||(s[0]=n("",10)),i(l,{title:"不同的数据结构",code:`graph LR
+A[线性表] --插入、删除
+只能在表的一端进行--> B[栈]
+A --插入、在表的一端进行
+删除只能在表的另一端进行--> C[队列]
+`}),s[1]||(s[1]=n("",83)),i(p),i(t,{color:"text-red-500",weight:500})])}const u=h(r,[["render",E]]);export{o as __pageData,u as default};
