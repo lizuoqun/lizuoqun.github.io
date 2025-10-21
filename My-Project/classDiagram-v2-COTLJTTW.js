f2d745c2b1270f012d8b27e4171ed88d56e6147f
modify <2645929971@qq.com>
2025-10-21 17:42:37 +0800

    ci(my-project): update & ci

--- My-Project/classDiagram-v2-COTLJTTW.js
+++
@@ -1,1 +0,0 @@
-import{s as a,c as s,a as e,C as t}from"./chunk-A2AXSNBT.js";import{_ as i}from"./theme.js";import"./chunk-RZ5BOZE2.js";import"./framework.js";var _={parser:e,get db(){return new t},renderer:s,styles:a,init:i(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{_ as diagram};
